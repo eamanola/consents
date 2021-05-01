30a908e033fac8cb297a5908fda3c050dfe36bc0
Ehsun Amanolahi <ehsun.amanolahi@gmail.com>
2021-05-01 12:33:43 +0300

    Use ListItems in Menu

--- src/App.js
+++ src/App.js
@@ -2,13 +2,11 @@ import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Grid from '@material-ui/core/Grid';
 import Container from '@material-ui/core/Container';
-// import { makeStyles } from '@material-ui/core/styles';
 
 import ResponsiveMenu from './components/responsive-menu';
 import Content from './components/content';
 import Menu from './components/menu';
 
-// const classes = makeStyles();
 const menu = <Menu />;
 
 const App = () => (
@@ -21,6 +19,7 @@ const App = () => (
         direction="row"
         justify="center"
         alignItems="center"
+        spacing={2}
       >
         <ResponsiveMenu menu={menu} />
         <Content />
--- src/components/menu.js
+++ src/components/menu.js
@@ -1,11 +1,11 @@
 import React from 'react';
-
 import { Link } from 'react-router-dom';
+import ListItem from '@material-ui/core/ListItem';
 
 const Menu = () => (
   <nav>
-    <Link to="/give-consent">Give consent</Link>
-    <Link to="/consents">Consents</Link>
+    <ListItem button component={Link} to="/give-consent">Give consent</ListItem>
+    <ListItem button component={Link} to="/consents">Consents</ListItem>
   </nav>
 );
 
